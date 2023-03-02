b2304a07a02e12f38f98cb2e232bc3e542956047
brad_fang <brad_fang@gss.com.tw>
2023-03-02 23:30:15 +0800

    ui init commit

---
+++ ui/layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <div class="container column is-12">
+        <Nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ ui/pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="section">
+    <div class="block">
+      <b-field>
+        <b-input
+          placeholder="paste url here ..."
+          size="is-medium"
+          icon="link"
+          icon-right="close-circle"
+          icon-right-clickable
+          @icon-right-click="clearUrl"
+          rounded>
+        </b-input>
+      </b-field>
+    </div>
+    <div class="buttons is-centered">
+      <b-button icon-left="zoom-in">
+        Generate Url
+      </b-button>
+    </div>
+    <div class="buttons is-centered">
+      <b-button icon-left="content-paste">
+        Paste From Clipboard And Generate Url
+      </b-button>
+    </div>
+    <div class="block">
+      <b-field>
+        <b-input
+          size="is-medium"
+          readonly
+          icon-right="content-copy"
+          icon-right-clickable
+          @icon-right-click="copyShortUrl"
+          rounded>
+        </b-input>
+      </b-field>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Card
+  },
+  data: {
+    url: '',
+    shortUrl: ''
+  },
+  methods: {
+    clearUrl() {
+      this.url = '';
+    },
+    copyShortUrl() {
+      this.email = '';
+      alert('Email cleared!')
+    }
+  }
+}
+</script>
